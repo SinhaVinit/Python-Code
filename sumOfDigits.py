22050b91435a7931d31b677d5c564fcb86adba1d
SinhaVinit <vinit.8444935532@gmail.com>
2022-02-08 23:19:57 +0530

    Modified sumOfDigits

--- sumOfDigits.py
+++ sumOfDigits.py
@@ -1,7 +1,7 @@
 def sumOfDigits(number):
-    number = int(number)
+    assert number >= 0 and int(number) == number, "The number has to be a positive only"
     if number < 1:
         return 0
-    return (number%10 + sumOfDigits(number/10))
+    return (int(number%10) + sumOfDigits(int(number/10)))
 
-print(sumOfDigits(121))
+print(sumOfDigits(1234321))
