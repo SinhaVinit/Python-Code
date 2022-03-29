903435dbb4cb8e8a20a4e9c0a39b50c966415947
SinhaVinit <vinit.8444935532@gmail.com>
2022-03-29 23:59:31 +0530

    Adding file in List folder

---
+++ List/List_listOperation_Function_1.py
@@ -0,0 +1,15 @@
+a = [1,2,3]
+b = [4,5,6,7]
+c = a + b
+print(c)
+
+d = [0,1]
+d = d * 4
+print(d)
+
+e = [0,1,2,3,4,5,6]
+print(len(e))
+print(max(e))
+print(min(e))
+print(sum(e))
+print(sum(e)/len(e))
