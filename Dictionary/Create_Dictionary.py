812752b893de7f12222597cd145716128c1a6f93
SinhaVinit <vinit.8444935532@gmail.com>
2022-04-05 20:55:30 +0530

    Creating Dictionary folder and adding filles on it. Also adding file to Arrays_Lists folder which is in Interview_Questions folder

---
+++ Dictionary/Create_Dictionary.py
@@ -0,0 +1,10 @@
+# Two ways to create a dictionary
+myDict1 = dict()
+myDict2 = {}
+print(myDict1)
+print(myDict2)
+
+myDict3 = {1:"one",2:"two"}
+print(myDict3)
+
+print(myDict3[1]) # ----------------------------> Time Complexity: O(1)
---
+++ Dictionary/Dictionary_Insert_Update_Element.py
@@ -0,0 +1,11 @@
+# Update
+myDict = {"name": "Vinit", "age": 22}
+myDict["age"] = 23
+print(myDict)
+
+# Insert
+myDict["address"] = "Kolkata,India"
+print(myDict)
+
+# Time Complexity: O(1)
+# Space Complexity: amortized O(1)
