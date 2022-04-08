086d33e3d0cc58c990c6ee2161187baabb9f6e76
SinhaVinit <vinit.8444935532@gmail.com>
2022-04-08 15:53:48 +0530

    Adding file in Dictionary folder

---
+++ Dictionary/Dictionary_Metods.py
@@ -0,0 +1,33 @@
+myDict = {"name": "Vinit", "age": 22, "address": "Kolkata,India", "education": "phd"}
+
+# myDict.clear()
+# print(myDict)
+
+# dict = myDict.copy()
+# print(myDict)
+# print(dict)
+
+# newDict = {}.fromkeys([1,2,3,4], 0)
+# print(newDict)
+
+# print(myDict.get("age", 23))
+# print(myDict.get("country", "India"))
+
+# print(myDict.items())
+
+# print(myDict.keys())
+
+# print(myDict.popitem())
+# print(myDict)
+
+# print(myDict.setdefault("age", 23))
+# print(myDict.setdefault("age1", 23))
+# print(myDict)
+
+# print(myDict.pop("age1", "not"))
+
+# print(myDict.values())
+
+newDict = {"a":1, "b":2, "c":3, "d":4, "age":21}
+myDict.update(newDict)
+print(myDict)
