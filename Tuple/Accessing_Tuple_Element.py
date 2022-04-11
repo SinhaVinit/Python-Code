366c225d2df3481bfece5247dfd4bfe011b5dbee
SinhaVinit <vinit.8444935532@gmail.com>
2022-04-11 23:23:35 +0530

    Adding file in the Tuple folder.

---
+++ Tuple/Accessing_Tuple_Element.py
@@ -0,0 +1,11 @@
+newTuple = ("a", "b", "c", "d", "e")
+
+print(newTuple[1])
+print(newTuple[-1])
+print(newTuple[-2])
+print(newTuple[1:3])
+print(newTuple[:3])
+print(newTuple[1:])
+print(newTuple[:])
+
+# newTuple[0] = "f" # This can not be done because tuple not allowed to update or change.
