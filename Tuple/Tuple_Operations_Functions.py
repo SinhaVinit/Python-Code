936543d15b8e8c9356b05c7b0fe6144ffac5bc43
SinhaVinit <vinit.8444935532@gmail.com>
2022-04-14 23:13:53 +0530

    Adding file in Tuple folder.

---
+++ Tuple/Tuple_Operations_Functions.py
@@ -0,0 +1,21 @@
+myTuple = (1,4,3,2,5,2)
+myTuple1 = (1,2,6,9,8,7)
+
+# print(myTuple + myTuple1)
+
+# print(myTuple * 4)
+
+# print(4 in myTuple)
+
+# print(myTuple.count(2))
+
+# print(myTuple.index(2))
+# print(myTuple.index(4))
+
+# print(len(myTuple))
+
+# print(max(myTuple))
+
+# print(min(myTuple))
+
+print(tuple([1,2,3,4,5,6,7,8,9,10]))
