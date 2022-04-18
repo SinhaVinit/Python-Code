7887e816984026aaf1193610bba406747cb6779f
SinhaVinit <vinit.8444935532@gmail.com>
2022-04-18 23:20:05 +0530

    modified: Linked_Lists/Insertion_Method_Singly_Linked_List.py ==== new file: Linked_Lists/Traversal_Singley_Linked_List.py

--- Linked_Lists/Traversal_Singley_Linked_List.py
+++ Linked_Lists/Traversal_Singley_Linked_List.py
@@ -15,31 +15,42 @@ class SLinkedList:
     # insert in Linked List
     def insertSLL(self, value, location):
         newNode = Node(value)
-        if self.head is None: #------------------------------------------> O(1)
+        if self.head is None:
             self.head = newNode
             self.tail = newNode
         else:
-            if location == 0: #------------------------------------------> O(1)
+            if location == 0:
                 newNode.next = self.head
                 self.head = newNode
-            elif location == -1: # elif location == 1: #-----------------> O(1)
+            elif location == -1:
                 newNode.next = None
                 self.tail.next = newNode
                 self.tail = newNode
             else:
                 tempNode = self.head
                 index = 0
-                while index < location - 1: #----------------------------> O(n)
+                while index < location - 1:
                     tempNode = tempNode.next
                     index += 1
-                nextNode = tempNode.next #-------------------------------> O(1)
-                tempNode.next = newNode #--------------------------------> O(1)
-                newNode.next = nextNode #--------------------------------> O(1)
-                if tempNode == self.tail: #------------------------------> O(1)
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+                if tempNode == self.tail:
                     self.tail = newNode
 
-# Time Complexity: O(n)
-# Space Complexity: O(1) , Because here we are just creating 2 nodes. The 1st node is newNode and 2nd node is tempNode. Thats why the spaxe complexity is O(1).
+    # traverse Singly Linked List
+    def traverseSLL(self):
+        if self.head is None: #------------------------------------------------> O(1)
+            print("The Singly Linked List does not exist") #-------------------> O(1)
+        else:
+            node = self.head #-------------------------------------------------> O(1)
+            while node is not None: #------------------------------------------> O(n)
+                print(node.value)
+                node = node.next
+        # For Traverse through Singly Linked List
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+
 
 singlyLinkedList = SLinkedList()
 singlyLinkedList.insertSLL(1, -1)
@@ -53,3 +64,4 @@ singlyLinkedList.insertSLL(10, 3)
 
 singlyLinkedList.insertSLL(20, 4)
 print([node.value for node in singlyLinkedList])
+singlyLinkedList.traverseSLL()
