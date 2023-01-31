6d0e7e8b69aa8a46355c653d43bd1ea86431700c
SinhaVinit <vinit.8444935532@gmail.com>
2023-01-31 23:15:35 +0530

    Left circular linked list and jumped to stack. CLL will cover later.

--- rough1.py
+++ rough1.py
@@ -1,2 +1,23 @@
-txt = "Hello\fWorld!"
-print(txt)
+def min_max_time(date):
+    lst = []
+    min_lst = []
+    max_lst =[]
+    for i in range(len(date)):
+        lst.append(date[i])
+    for j in range(len(lst)):
+        if lst[j] == " ":
+            if j == 0 or j == 3 or j == 6:
+                min_lst.append("0")
+                max_lst.append("5")
+            elif j == 1 or j == 4 or j == 7:
+                min_lst.append("0")
+                max_lst.append("9")
+        else:
+            min_lst.append(lst[j])
+            max_lst.append(lst[j])
+    min_time = "".join(min_lst)
+    max_time = "".join(max_lst)
+    return (min_time, max_time)
+
+date = str(input())
+print(min_max_time(date))
